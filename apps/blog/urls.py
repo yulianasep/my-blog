b594957f126565ecb4e1103ca19c402b6fd50060
yulianasep <yuliana.sepulveda@alternova.com>
2023-07-10 18:44:51 -0500

    feat: create post and list post

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_view, Post
+from .views import home_view, CreatePostView, PostListView
 
 urlpatterns = [
-    path('/', home_view),
-    path('/post', Post.as_view()),
+    path('', home_view),
+    path('post/create/', CreatePostView.as_view()),
+    path('post/list/', PostListView.as_view()),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView
 
+from apps.blog import models
+
 # Create your views here.
 
 def home_view(request):
-    print("Hola")
     return render(request,"home.html")
 
+class PostListView(ListView):
+    template_name = "PostListView.html"
+    model = models.Post
+    context_object_name = 'post_list'
 
-class Post(TemplateView):
-    template_name = "post.html"
-
-
-
-
+class CreatePostView(CreateView):
+    template_name = "CreatePostView.html"
+    fields = '__all__'
+    model = models.Post
+    success_url = ''
